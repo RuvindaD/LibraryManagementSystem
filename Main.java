26419829c943d049f4c4f61e0da3d8c90a392587
RuvindaD <rdilleperuma@gmail.com>
2024-08-01 22:25:20 +0530

    main

---
+++ Main.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package edu.ijse.layered;
+
+import edu.ijse.layered.view.BookCategoriesView;
+import edu.ijse.layered.view.BooksView;
+import edu.ijse.layered.view.BorrowingView;
+import edu.ijse.layered.view.HomepageView;
+import edu.ijse.layered.view.LoginView;
+import edu.ijse.layered.view.MembersView;
+import edu.ijse.layered.view.TransactionsView;
+
+/**
+ *
+ * @author Admin
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws Exception {
+//       new BookCategoriesView().setVisible(true);
+//       new BooksView().setVisible(true);
+//       new MembersView().setVisible(true);
+//       new TransactionsView().setVisible(true);
+      // new BorrowingView().setVisible(true);
+       new LoginView().setVisible(true);
+      // new HomepageView().setVisible(true);
+      
+    }
+    
+}
