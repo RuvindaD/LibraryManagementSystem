74fd01abb657dfeab7f6c33e8726a7ae4de5349c
RuvindaD <rdilleperuma@gmail.com>
2024-08-01 22:32:26 +0530

    dao

---
+++ BookCategoriesDao.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package edu.ijse.layered.dao.custom;
+
+import edu.ijse.layered.dao.CrudDao;
+import edu.ijse.layered.entity.BookCategoriesEntity;
+
+/**
+ *
+ * @author Admin
+ */
+public interface BookCategoriesDao extends CrudDao<BookCategoriesEntity, String> {
+    
+    
+}
